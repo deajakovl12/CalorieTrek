b844bf59f8a9ac6a6b21fa103874214ad325fc89
fillevaci <levacic1995@gmail.com>
2018-01-03 23:41:35 +0100

    I added a menu in FinishedTraining as the first step in adding "finished training details" modules. Both the class and the .xml thus had to be changed. A new menu was created in the appropriate map. Several strings were added to better describe the action on the menu. Search bar was not added since it is not necessary in adding modularity. Since I lack the correct icons, I put temporary ones into place. They don't do anything yet, this is only a small commit to prepare for modularity implementation using the submenu.

--- app/src/main/java/foi/hr/calorietrek/ui/finished_training/FinishedTraining.java
+++ app/src/main/java/foi/hr/calorietrek/ui/finished_training/FinishedTraining.java
@@ -1,15 +1,43 @@
 package foi.hr.calorietrek.ui.finished_training;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import foi.hr.calorietrek.R;
+import foi.hr.calorietrek.ui.profile.view.ProfileActivity;
+import foi.hr.calorietrek.ui.training.view.TrainingActivity;
 
 public class FinishedTraining extends AppCompatActivity {
+
+    public @BindView(R.id.toolbarDetails) Toolbar toolbarDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finished_training);
+
+        ButterKnife.bind(this);
+        toolbarDetails.setTitle("");
+        setSupportActionBar(toolbarDetails);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_finished_training, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
     }
 }
