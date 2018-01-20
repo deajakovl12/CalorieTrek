d80e658341c6e7d95209278f6d9797cd68e42ba1
tomvargek <tomvargek@foi.hr>
2018-01-20 14:28:17 +0100

    Google+ Share
    User can now share training result to Google+. User shares graph image to Google+. Graph image shows his overall training result. In order to share to Google+, user must install Google+ app.

--- app/src/main/java/foi/hr/calorietrek/ui/finished_training/FinishedTraining.java
+++ app/src/main/java/foi/hr/calorietrek/ui/finished_training/FinishedTraining.java
@@ -1,12 +1,20 @@
 package foi.hr.calorietrek.ui.finished_training;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.SubMenu;
+import android.widget.Toast;
+
+import com.google.android.gms.plus.PlusShare;
 
 import java.util.ArrayList;
 
@@ -25,6 +33,8 @@ public class FinishedTraining extends AppCompatActivity {
     private NavigationManager navManager;
     private DbHelper instance;
     public ArrayList<TrainingModel> allTrainings;
+    private static final int REQ_SELECT_PHOTO = 1;
+    private static final int REQ_START_SHARE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,9 +91,55 @@ public class FinishedTraining extends AppCompatActivity {
                 break;
             case R.id.action_module_submenu:
                 break;
+
+            case R.id.action_share_google:
+                boolean isAppInstalled = appInstalledOrNot("com.google.android.apps.plus");
+                if(isAppInstalled) {
+                    Intent photoPicker = new Intent(Intent.ACTION_PICK);
+                    photoPicker.setType("video/*, image/*");
+                    startActivityForResult(photoPicker, REQ_SELECT_PHOTO);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Google+ app not installed!", Toast.LENGTH_SHORT).show();
+                }
+                break;
             default:
                 navManager.selectNavigationItem(item);
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if(requestCode == REQ_SELECT_PHOTO) {
+            if(resultCode == RESULT_OK) {
+                Uri selectedImage = intent.getData();
+                ContentResolver cr = this.getContentResolver();
+                String mime = cr.getType(selectedImage);
+
+
+                PlusShare.Builder share = new PlusShare.Builder(this);
+                share.setText("My CalorieTrek Training!");
+                share.addStream(selectedImage);
+                share.setType(mime);
+                startActivityForResult(share.getIntent(), REQ_START_SHARE);
+            }
+        }
+    }
+
+
+    private boolean appInstalledOrNot(String uri) {
+        PackageManager pm = getPackageManager();
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+
+        return false;
+    }
+
+
+
 }
